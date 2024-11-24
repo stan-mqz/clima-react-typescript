7db4462ea692dff0f8ae9d86ce9cb593585870b4
Stan <moscarstanley37@gmail.com>
2024-11-23 21:54:22 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import styles from './App.module.css'
+
+function App() {
+
+
+
+  return (
+    <>
+    <h1 className={styles.title}>Buscador de clima</h1>
+    <div className={styles.container}>
+      <p>1</p>
+      <p>2</p>
+    </div>
+    </>
+  )
+}
+
+export default App
