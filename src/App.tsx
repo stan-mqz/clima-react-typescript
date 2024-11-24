1a494fad75acf2f8280cfb6e43c7e823793d4068
Stan <moscarstanley37@gmail.com>
2024-11-23 22:05:18 -0600

    Created Form Component and countries.ts to get a select with a list of countries so we can obtain their whether

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css'
+import { Form } from './components/Form/Form'
 
 function App() {
 
@@ -8,8 +9,8 @@ function App() {
     <>
     <h1 className={styles.title}>Buscador de clima</h1>
     <div className={styles.container}>
-      <p>1</p>
-      <p>2</p>
+    <Form />
+    <p>2</p>
     </div>
     </>
   )
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { countries } from "../../data/countries"
 
 export const Form = () => {
   return (
-    <div>Form</div>
-  )
+    <form>
+        <div className="">
+            <label htmlFor="city">Ciudad:</label>
+            <input type="text" name="city " id="city"  placeholder="Ciudad"/>
+        </div>
+
+        <div className="">
+            <label htmlFor="country">Pais</label>
+
+            <select name="city" id="">
+                <option value="">-- Seleccione un país -- </option> 
+                {countries.map(country => (
+                    <option key={country.code} value={country.code}>{country.name}</option>
+                ))}
+            </select>
+
+            <input type="submit" value='Consultar clima' />
+        </div>
+    </form>
+)
 }
